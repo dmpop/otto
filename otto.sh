c80e3e934d36da04bd841f655622bd96dd849806
Dmitri Popov <dmpop@linux.com>
2020-06-17 17:21:39 +0200

    Major improvements

--- otto.sh
+++ otto.sh
@@ -1,40 +1,59 @@
 #!/usr/bin/env bash
 
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
 # Author: Dmitri Popov, dmpop@linux.com
-# License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt
 # Source code: https://gitlab.com/dmpop/otto
 
 # On a NAS running Entware, run the following command
-# opkg install getopt bc jq curl perl-image-exiftool
+# opkg install getopt bc jq curl perl-image-exiftool rsync
 
 # Check whether the required packages are installed
-if [ ! -x "$(command -v getopt)" ] || [ ! -x "$(command -v bc)" ] || [ ! -x "$(command -v jq)" ] || [ ! -x "$(command -v curl)" ] || [ ! -x "$(command -v exiftool)" ]; then
-    echo "Make sure that the following tools are installed on your system: getopt, bc, jq, curl, exiftool"
+if [ ! -x "$(command -v getopt)" ] || [ ! -x "$(command -v bc)" ] || [ ! -x "$(command -v jq)" ] || [ ! -x "$(command -v curl)" ] || [ ! -x "$(command -v exiftool)"] || [ ! -x "$(command -v rsync)" ]; then
+    echo "Make sure that the following tools are installed on your system: getopt, bc, jq, curl, exiftool, rsync"
     exit 1
 fi
-echo
-echo "-----------------------------------"
-echo "        Hello! I'm Otto."
-echo "Let's transfer and organize photos!"
-echo "-----------------------------------"
-echo
+
 # Usage prompt
 usage(){
 cat <<EOF
 $0 [OPTIONS]
-
+------
 $0 imports, geotags, adds metadata, and organizes photos and RAW files.
 
-Usage:
+USAGE:
+------
   $0 -d <dir> -g <location> -c <dir>
 
-Options:
+OPTIONS:
+--------
   -d Specifies the source directory
   -g Geotag using coordinates of the specified location (city)
   -c path to a directory containing one or several GPX files (optional)
 EOF
   exit 1
 }
+
+echo
+echo "-----------------------------------------"
+echo "          Hello! I'm Otto.   "
+echo "   Let's transfer and organize photos!   "
+echo "-----------------------------------------"
+echo
+
 # Obtain values
 while getopts "d:g:c:" opt; do
   case ${opt} in
@@ -51,20 +70,13 @@ done
 shift $((OPTIND -1))
 CONFIG_DIR=$(dirname "$0")
 CONFIG="${CONFIG_DIR}/otto.cfg"
-#Check whether the path to the source directory is specified
+
+# Check whether the path to the source directory is specified
 if [ -z "$src" ]; then
-    echo "Please specify the path to the source directory"
+    usage
     exit 1
     fi
-# Check whether the Photon service is reachable
-check=$(wget -q --spider http://photon.komoot.de/)
-if [ ! -z "$check" ]; then
-    echo
-    echo "--------------------------------------------------------"
-    echo "Photon is not reachable. Check your Internet connection."
-    echo "--------------------------------------------------------"
-    exit 1
-    fi
+
 # Ask for the required info and write the obtained values into the configuration file
 if [ ! -f "$CONFIG" ]; then
     echo "Specify destination directory and press [ENTER]:"
@@ -88,105 +100,34 @@ if [ ! -f "$CONFIG" ]; then
     read password
     echo 'PASSWORD="'$password'"' >> "$CONFIG"
     fi
+
 source "$CONFIG"
 mkdir -p "$TARGET"
+
 echo
-echo "---------------------"
-echo "Transferring files..."
-echo "---------------------"
+echo "----------------------------"
+echo "   Transferring files ...   "
+echo "----------------------------"
 echo
-results=$(find "$src" -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d":" -f1)
-lines=$(echo -e "$results" | wc -l)
-for line in $(seq 1 $lines)
-do
-    file=$(echo -e "$results" | sed -n "$line p")
-    echo "$file"
-    cp "$file" "$TARGET"
-done
+
+rsync -avh --delete "$src" "$TARGET"
+
 cd "$TARGET"
-if [ ! -z "$location" ]; then
-    # Obtain latitude and longitude for the specified location
-    lat=$(curl -k "photon.komoot.de/api/?q=$location" | jq '.features | .[0] | .geometry | .coordinates | .[1]')
-    if (( $(echo "$lat > 0" |bc -l) )); then
-        latref="N"
-        else
-        latref="S"
-    fi
-    lon=$(curl -k "photon.komoot.de/api/?q=$location" | jq '.features | .[0] | .geometry | .coordinates | .[0]')
-    
-    if (( $(echo "$lon > 0" |bc -l) )); then
-        lonref="E"
-        else
-        lonref="W"
-    fi
-fi
-# Geotag if the -g parameter is not empty
-if [ ! -z "$location" ]; then
-    echo
-    echo "--------------"
-    echo "Geotagging ..."
-    echo "--------------"
-    echo
-    results=$(find "$TARGET" -name '*' -exec file {} \; | grep -o -P '^.+: JPEG' | cut -d":" -f1)
-    lines=$(echo -e "$results" | wc -l)
-    for line in $(seq 1 $lines)
-        do
-            file=$(echo -e "$results" | sed -n "$line p")
-            exiftool -overwrite_original -GPSLatitude=$lat -GPSLatitudeRef=$latref -GPSLongitude=$lon -GPSLongitudeRef=$lonref "$file"
-        done
-        fi
-if [ ! -z "$gpx" ]; then
-    # Count GPX files in the specified directory
-    cd "$gpx"
-    fcount=$(ls -1 | wc -l)
-    # Check for GPX files and GPSBabel
-    if [ "$fcount" -eq "0" ]; then
-	echo "No GPX files are found."
-	exit 1
-    fi
-    # Check whether the required packages are installed
-    if [ ! -x "$(command -v gpsbabel)" ]; then
-	echo "GPSBabel is not found."
-	exit 1
-    fi
-    # Geocorrelate with a single GPX file
-    if [ "$fcount" -eq "1" ]; then
-	echo
-	echo "--------------"
-	echo "Geotagging ..."
-	echo "--------------"
-	echo
-	fgpx=$(ls "$gpx")
-	exiftool -overwrite_original -r -geotag "$fgpx" -geosync=180 "$TARGET"
-    fi
-    if [ "$fcount" -gt "1" ]; then
-	echo
-	echo "---------------------"
-	echo "Merging GPX files ..."
-	echo "---------------------"
-	echo
-	cd "$gpx"
-	ff=""
-	for f in *.gpx
-	do
-	    ff="$ff -f $f"
-	done
-	gpsbabel -i gpx $ff -o gpx -F "output.gpx"
-	fgpx=$(pwd)"/output.gpx"
-	exiftool -overwrite_original -r -geotag "$fgpx" -geosync=180 "$TARGET"
-    fi
-fi
-cd "$TARGET"
-echo "-------------------------"
-echo "Writing EXIF metadata ..."
-echo "-------------------------"
+
+echo "------------------------"
+echo "   Renaming files ...   "
+echo "------------------------"
+echo
+
+exiftool -d %Y%m%d-%H%M%S%%-c.%%e '-FileName<DateTimeOriginal' -directory="$TARGET" -r .
+
+echo "-------------------------------"
+echo "   Writing EXIF metadata ...   "
+echo "-------------------------------"
 echo
 # Obtain and write copyright camera model, lens, and weather info
-results=$(find "$TARGET" -name '*' -exec file {} \; | grep -o -P '^.+: JPEG' | cut -d":" -f1)
-lines=$(echo -e "$results" | wc -l)
-for line in $(seq 1 $lines)
+for file in *.*
 do
-    file=$(echo -e "$results" | sed -n "$line p")
     date=$(exiftool -DateTimeOriginal -d %Y-%m-%d "$file" | cut -d":" -f2 | tr -d " ")
     wf=$date".txt"
     if [ ! -z "$FTP" ]; then
@@ -203,21 +144,98 @@ do
     lens=$(exiftool -LensID "$file" | cut -d":" -f2)
     exiftool -overwrite_original -copyright="$copyright" -comment="$camera $lens $weather" "$file"
 done
+
+if [ ! -z "$location" ]; then
+# Check whether the Photon service is reachable
+    check=$(wget -q --spider http://photon.komoot.de/)
+    if [ ! -z "$check" ]; then
+	echo
+	echo "--------------------------------------------------------------"
+	echo "   Photon is not reachable. Check your Internet connection.   "
+	echo "                  Geotagging skipped.                         "
+	echo "--------------------------------------------------------------"
+    else
+	# Obtain latitude and longitude for the specified location
+	lat=$(curl -k "photon.komoot.de/api/?q=$location" | jq '.features | .[0] | .geometry | .coordinates | .[1]')
+	if (( $(echo "$lat > 0" |bc -l) )); then
+            latref="N"
+        else
+            latref="S"
+	fi
+	lon=$(curl -k "photon.komoot.de/api/?q=$location" | jq '.features | .[0] | .geometry | .coordinates | .[0]')
+	if (( $(echo "$lon > 0" |bc -l) )); then
+            lonref="E"
+        else
+            lonref="W"
+	fi
+	echo
+	echo "--------------------"
+	echo "   Geotagging ...   "
+	echo "--------------------"
+	echo
+	exiftool -overwrite_original -GPSLatitude=$lat -GPSLatitudeRef=$latref -GPSLongitude=$lon -GPSLongitudeRef=$lonref -r .
+    fi
+fi
+
+if [ ! -z "$gpx" ]; then
+    # Count GPX files in the specified directory
+    cd "$gpx"
+    fcount=$(ls -1 | wc -l)
+    # Check for GPX files and GPSBabel
+    if [ "$fcount" -eq "0" ]; then
+	echo "No GPX files are found."
+	exit 1
+    fi
+    # Check whether the required packages are installed
+    if [ ! -x "$(command -v gpsbabel)" ]; then
+	echo "----------------------------"
+	echo "   GPSBabel is not found.   "
+	echo "    Geotagging skipped.     "
+	echo "----------------------------"
+	exit 1
+    fi
+    # Geocorrelate with a single GPX file
+    if [ "$fcount" -eq "1" ]; then
+	echo
+	echo "--------------------"
+	echo "   Geotagging ...   "
+	echo "--------------------"
+	echo
+	fgpx=$(ls "$gpx")
+	exiftool -overwrite_original -r -geotag "$fgpx" -geosync=180 -r .
+    fi
+    if [ "$fcount" -gt "1" ]; then
+	echo
+	echo "---------------------------"
+	echo "   Merging GPX files ...   "
+	echo "---------------------------"
+	echo
+	cd "$gpx"
+	ff=""
+	for f in *.gpx
+	do
+	    ff="$ff -f $f"
+	done
+	gpsbabel -i gpx $ff -o gpx -F "output.gpx"
+	fgpx=$(pwd)"/output.gpx"
+	exiftool -overwrite_original -r -geotag "$fgpx" -geosync=180 -r .
+    fi
+fi
+
 echo
 echo "--------------------------"
-echo "Renaming and organizing..."
+echo "   Organizing files ...   "
 echo "--------------------------"
 echo
-exiftool -d %Y%m%d-%H%M%S%%-c.%%e '-FileName<DateTimeOriginal' "$TARGET"
-exiftool '-Directory<CreateDate' -d ./%Y-%m-%d "$TARGET"
+exiftool '-Directory<CreateDate' -d ./%Y-%m-%d -r .
 cd
 # find "$TARGET" -type d -exec chmod 755 {} \;
 if [ ! -z "$NOTIFY_TOKEN" ]; then
     curl "https://api.simplepush.io/send/${NOTIFY_TOKEN}/Otto/All done!"
 else
     echo
-    echo "---------"
-    echo "All done!"
-    echo "---------"
+    echo "---------------"
+    echo "   All done!   "
+    echo "---------------"
     echo
 fi
